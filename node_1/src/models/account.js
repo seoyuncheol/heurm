a1227ea53d7e5d2047c4b8fe403c66db494bdb74
yuncheol <yuncheol92@naver.com>
2019-03-14 00:50:44 +0900

    account.js

--- node_1/src/models/account.js
+++ node_1/src/models/account.js
@@ -72,5 +72,9 @@ Account.methods.generateToken = function() {
 
   return generateToken(payload);
 };
+Account.methods.increaseThoughtCount = function() {
+  this.thoughtCount++;
+  return this.save();
+}
 
 module.exports = mongoose.model("Account", Account);
